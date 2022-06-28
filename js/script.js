3b7d26790ec60f72efb0788a39842eed63f5d620
Steveh77 <stederosa76@gmail.com>
2022-06-28 17:11:21 +0200

    esercizio con ciclo WHILE

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ let listaSpesa = ['Uova' , 'Pane' , 'Latte' , 'Biscotti' , 'Pasta' , 'Affettati'
 const ulListContainer = document.getElementById("ul-list-container");
 
 // ---------esercizio con ciclo FOR--------------
+// let liListItems = '';
+// for (let i = 0; i < listaSpesa.length; i++) {
+//     liListItems +=`<li>${listaSpesa[i]}</li>`;
+//     console.log(i)
+// }
+
+// ---------esercizio con ciclo WHILE--------------
 let liListItems = '';
-for (let i = 0; i < listaSpesa.length; i++) {
+let i = 0; 
+while (i < listaSpesa.length) {
     liListItems +=`<li>${listaSpesa[i]}</li>`;
+    i++;
     console.log(i)
 }
 
